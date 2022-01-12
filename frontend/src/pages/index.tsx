ce5014d556f624a671096c4248fe6c2f868e8dd5
DragutuMatei <67979189+DragutuMatei@users.noreply.github.com>
2022-01-12 03:04:25 +0200

    front page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -6,56 +6,140 @@ import { createUrqlClient } from "../utils/createUrqlClient";
 import NextLink from "next/link";
 import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
 import { IconButton } from "@chakra-ui/react";
-
+var i = 0;
 function Index() {
-  const [variables, setVariables] = useState({
-    limit: 5,
-    cursor: undefined as string | undefined,
-  });
+  const prev = () => {
+    const elem = document.querySelector(".slider-cards") as HTMLAnchorElement;
+    i--;
+    elem.style.left = 255 * -i + "px";
+  };
 
-  // const [{ data, fetching }] = usePostsQuery({ variables });
+  const next = () => {
+    const elem = document.querySelector(".slider-cards") as HTMLAnchorElement;
+    i++;
+    let l = i == 1 ? 270 : 260;
+    elem.style.left = l * -i + "px";
+  };
 
   return (
     <Layout>
       <div className="components">
         <div className="container">
-          <div className="text-container">
-            <h1>NEVER STOP EXPLORING THE WORLD.</h1>
-            <h3>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              sunt in culpa qui officia deserunt mollit anim id est laborum.{" "}
-            </h3>
-            <button>Click me !</button>
-          </div>
+          <h1>NEVER STOP EXPLORING THE WORLD</h1>
+          <h3>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do sunt
+            in culpa qui officia deserunt mollit anim id est laborum.
+          </h3>
+          <button>Click me !</button>
         </div>
         <div className="slider">
           <div className="slider-cards">
-          <div className="slider-card">
-            <h3 className="slider-card-title">Task #1</h3>
-            <h5 className="slider-card-subtitle">Something should be here</h5>
-            <p className="slider-card-description">
-              nush ce sa zic aici dar trb sa fie
-            </p>
-          </div>
-          <div className="slider-card">
-            <h3 className="slider-card-title">Task #2</h3>
-            <h5 className="slider-card-subtitle">Something should be here</h5>
-            <p className="slider-card-description">
-              nush ce sa zic aici dar trb sa fie
-            </p>
-          </div>
-          <div className="slider-card">
-            <h3 className="slider-card-title">Task #3</h3>
-            <h5 className="slider-card-subtitle">Something should be here</h5>
-            <p className="slider-card-description">
-              nush ce sa zic aici dar trb sa fie
-            </p>
-          </div>
+            <div className="slider-card">
+              <h1>ceva scris1</h1>
+              <img src="/images/undraw_reminders_re_gtyb.svg" alt="" />
+            </div>
+            <div className="slider-card">
+              <h1>ceva scris2</h1>
+              <img src="/images/undraw_add_information_j2wg.svg" alt="" />
+            </div>
+            <div className="slider-card">
+              <h1>ceva scris3</h1>
+              <img src="/images/undraw_filter_re_sa16.svg" alt="" />
+            </div>
+            <div className="slider-card">
+              <h1>ceva scris4</h1>
+              <img src="/images/undraw_filter_re_sa16.svg" alt="" />
+            </div>
+            <div className="slider-card">
+              <h1>ceva scris5</h1>
+              <img src="/images/undraw_filter_re_sa16.svg" alt="" />
+            </div>
+            <div className="slider-card">
+              <h1>ceva scris6</h1>
+              <img src="/images/undraw_filter_re_sa16.svg" alt="" />
+            </div>
+            <div className="slider-card">
+              <h1>ceva scris7</h1>
+              <img src="/images/undraw_filter_re_sa16.svg" alt="" />
+            </div>
+            <div className="slider-card">
+              <h1>ceva scris8</h1>
+              <img src="/images/undraw_filter_re_sa16.svg" alt="" />
+            </div>
+            <div className="slider-card">
+              <h1>ceva scris9</h1>
+              <img src="/images/undraw_filter_re_sa16.svg" alt="" />
+            </div>
           </div>
           <div className="buttons">
-          <button className="slider-btn-left">Prev</button>
-          <button className="slider-btn-right">Next</button>
-          <span>0/1</span>
+            <button
+              className="slider-btn-left"
+              onClick={() => {
+                prev();
+              }}
+            >
+              Prev
+            </button>
+            <button
+              className="slider-btn-right"
+              onClick={() => {
+                next();
+              }}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="texte">
+        <div className="section">
+          <div className="left">
+            <img src="/images/undraw_pair_programming_re_or4x.svg" alt="" />
+          </div>
+          <div className="right">
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem
+              facilis laboriosam, ipsa sint soluta non dicta reprehenderit quos
+              voluptates blanditiis. Ab cupiditate, quia voluptates aperiam
+              ipsum placeat animi quas aspernatur!
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem
+              facilis laboriosam, ipsa sint soluta non dicta reprehenderit quos
+              voluptates blanditiis. Ab cupiditate, quia voluptates aperiam
+              ipsum placeat animi quas aspernatur!
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem
+              facilis laboriosam, ipsa sint soluta non dicta reprehenderit quos
+              voluptates blanditiis. Ab cupiditate, quia voluptates aperiam
+              ipsum placeat animi quas aspernatur!
+            </p>
+          </div>
+        </div>
+        <div className="section">
+          <div className="left">
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem
+              facilis laboriosam, ipsa sint soluta non dicta reprehenderit quos
+              voluptates blanditiis. Ab cupiditate, quia voluptates aperiam
+              ipsum placeat animi quas aspernatur!
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem
+              facilis laboriosam, ipsa sint soluta non dicta reprehenderit quos
+              voluptates blanditiis. Ab cupiditate, quia voluptates aperiam
+              ipsum placeat animi quas aspernatur!
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem
+              facilis laboriosam, ipsa sint soluta non dicta reprehenderit quos
+              voluptates blanditiis. Ab cupiditate, quia voluptates aperiam
+              ipsum placeat animi quas aspernatur!
+            </p>
+          </div>
+          <div className="right">
+            <img src="/images/undraw_pair_programming_re_or4x.svg" alt="" />
           </div>
         </div>
       </div>
